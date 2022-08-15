46aedc1d1823d08b9f029f629e6727a310284a1b
blinkhash <blinkhashpool@gmail.com>
2022-08-15 16:29:45 -0400

    Fix issue with transactions coinbase, small updates

--- stratum/test/transactions.test.js
+++ stratum/test/transactions.test.js
@@ -23,11 +23,10 @@ const extraNonce = Buffer.from('f000000ff111111f', 'hex');
 
 describe('Test transactions functionality', () => {
 
-  let configCopy, rpcDataCopy, rpcDataTransactionsCopy;
+  let configCopy, rpcDataCopy;
   beforeEach(() => {
     configCopy = JSON.parse(JSON.stringify(config));
     rpcDataCopy = JSON.parse(JSON.stringify(testdata.getBlockTemplate()));
-    rpcDataTransactionsCopy = JSON.parse(JSON.stringify(testdata.getBlockTemplateWithTransactions()));
   });
 
   test('Test main transaction builder [1]', () => {
@@ -90,67 +89,5 @@ describe('Test transactions functionality', () => {
     const transaction = new Transactions(configCopy, rpcDataCopy).handleGeneration(extraNonce);
     expect(transaction[0].slice(0, -5)).toStrictEqual(Buffer.from('01000000010000000000000000000000000000000000000000000000000000000000000000ffffffff0f5104', 'hex'));
     expect(transaction[1]).toStrictEqual(Buffer.from('0000000002902f5009000000001976a914b92c9db55f187b0d7f11e7c2d82e1cdc74301ce388ac0000000000000000266a24aa21a9ede2f61c3f71d1defd3fa999dfa36953755c690689799962b48bebd836974e8cf900000000', 'hex'));
-  });
-
-  test('Test main transaction builder w/ transactions [1]', () => {
-    const transaction = new Transactions(configCopy, rpcDataTransactionsCopy).handleGeneration(extraNonce);
-    expect(transaction[0].slice(0, -5)).toStrictEqual(Buffer.from('01000000010000000000000000000000000000000000000000000000000000000000000000ffffffff0f5104', 'hex'));
-    expect(transaction[1]).toStrictEqual(Buffer.from('0000000002a0565009000000001976a91459d584c2da3735f24af4ed3eb8e2abeb63fbffd688ac0000000000000000266a24aa21a9ede2f61c3f71d1defd3fa999dfa36953755c690689799962b48bebd836974e8cf900000000', 'hex'));
-  });
-
-  test('Test main transaction builder w/ transactions [2]', () => {
-    rpcDataTransactionsCopy.coinbasetxn = {};
-    rpcDataTransactionsCopy.coinbasetxn.data = '0500008085202';
-    const transaction = new Transactions(configCopy, rpcDataTransactionsCopy).handleGeneration(extraNonce);
-    expect(transaction[0].slice(0, -5)).toStrictEqual(Buffer.from('05000080010000000000000000000000000000000000000000000000000000000000000000ffffffff0f5104', 'hex'));
-    expect(transaction[1]).toStrictEqual(Buffer.from('0000000002a0565009000000001976a91459d584c2da3735f24af4ed3eb8e2abeb63fbffd688ac0000000000000000266a24aa21a9ede2f61c3f71d1defd3fa999dfa36953755c690689799962b48bebd836974e8cf900000000', 'hex'));
-  });
-
-  test('Test main transaction builder w/ transactions [3]', () => {
-    configCopy.primary.recipients.push({ address: 'RHUC17zAVjNqXDtkqwLPRvQ2XgoRZsXeeG', percentage: 0.05 });
-    const transaction = new Transactions(configCopy, rpcDataTransactionsCopy).handleGeneration(extraNonce);
-    expect(transaction[0].slice(0, -5)).toStrictEqual(Buffer.from('01000000010000000000000000000000000000000000000000000000000000000000000000ffffffff0f5104', 'hex'));
-    expect(transaction[1]).toStrictEqual(Buffer.from('0000000003181fd908000000001976a91459d584c2da3735f24af4ed3eb8e2abeb63fbffd688ac88377700000000001976a91459d584c2da3735f24af4ed3eb8e2abeb63fbffd688ac0000000000000000266a24aa21a9ede2f61c3f71d1defd3fa999dfa36953755c690689799962b48bebd836974e8cf900000000', 'hex'));
-  });
-
-  test('Test main transaction builder w/ transactions [4]', () => {
-    configCopy.primary.recipients.push({ address: 'RHUC17zAVjNqXDtkqwLPRvQ2XgoRZsXeeG', percentage: 0.05 });
-    configCopy.primary.recipients.push({ address: 'RHUC17zAVjNqXDtkqwLPRvQ2XgoRZsXeeG', percentage: 0.05 });
-    const transaction = new Transactions(configCopy, rpcDataTransactionsCopy).handleGeneration(extraNonce);
-    expect(transaction[0].slice(0, -5)).toStrictEqual(Buffer.from('01000000010000000000000000000000000000000000000000000000000000000000000000ffffffff0f5104', 'hex'));
-    expect(transaction[1]).toStrictEqual(Buffer.from('000000000490e76108000000001976a91459d584c2da3735f24af4ed3eb8e2abeb63fbffd688ac88377700000000001976a91459d584c2da3735f24af4ed3eb8e2abeb63fbffd688ac88377700000000001976a91459d584c2da3735f24af4ed3eb8e2abeb63fbffd688ac0000000000000000266a24aa21a9ede2f61c3f71d1defd3fa999dfa36953755c690689799962b48bebd836974e8cf900000000', 'hex'));
-  });
-
-  test('Test main transaction builder w/ transactions [5]', () => {
-    rpcDataTransactionsCopy.coinbaseaux.flags = 'test';
-    const transaction = new Transactions(configCopy, rpcDataTransactionsCopy).handleGeneration(extraNonce);
-    expect(transaction[0].slice(0, -5)).toStrictEqual(Buffer.from('01000000010000000000000000000000000000000000000000000000000000000000000000ffffffff0f5104', 'hex'));
-    expect(transaction[1]).toStrictEqual(Buffer.from('0000000002a0565009000000001976a91459d584c2da3735f24af4ed3eb8e2abeb63fbffd688ac0000000000000000266a24aa21a9ede2f61c3f71d1defd3fa999dfa36953755c690689799962b48bebd836974e8cf900000000', 'hex'));
-  });
-
-  test('Test main transaction builder w/ transactions [6]', () => {
-    delete rpcDataTransactionsCopy.default_witness_commitment;
-    const transaction = new Transactions(configCopy, rpcDataTransactionsCopy).handleGeneration(extraNonce);
-    expect(transaction[0].slice(0, -5)).toStrictEqual(Buffer.from('01000000010000000000000000000000000000000000000000000000000000000000000000ffffffff0f5104', 'hex'));
-    expect(transaction[1]).toStrictEqual(Buffer.from('0000000001a0565009000000001976a91459d584c2da3735f24af4ed3eb8e2abeb63fbffd688ac00000000', 'hex'));
-  });
-
-  test('Test main transaction builder w/ transactions [7]', () => {
-    rpcDataTransactionsCopy.auxData = auxiliaryData;
-    configCopy.auxiliary = auxiliaryConfig;
-    configCopy.auxiliary.enabled = true;
-    const transaction = new Transactions(configCopy, rpcDataTransactionsCopy).handleGeneration(extraNonce);
-    expect(transaction[0].slice(0, 44)).toStrictEqual(Buffer.from('01000000010000000000000000000000000000000000000000000000000000000000000000ffffffff3b5104', 'hex'));
-    expect(transaction[0].slice(49, 53)).toStrictEqual(Buffer.from('fabe6d6d', 'hex'));
-    expect(transaction[0].slice(53)).toStrictEqual(Buffer.from('17a35a38e70cd01488e0d5ece6ded04a9bc8125865471d36b9d5c47a08a5907c0100000000000000', 'hex'));
-    expect(transaction[1]).toStrictEqual(Buffer.from('0000000002a0565009000000001976a91459d584c2da3735f24af4ed3eb8e2abeb63fbffd688ac0000000000000000266a24aa21a9ede2f61c3f71d1defd3fa999dfa36953755c690689799962b48bebd836974e8cf900000000', 'hex'));
-  });
-
-  test('Test main transaction builder w/ transactions [8]', () => {
-    configCopy.settings.testnet = true;
-    configCopy.primary.address = 'mxQ4kX7k8c84JgNb6p8kbmxZD889YytnDT';
-    const transaction = new Transactions(configCopy, rpcDataTransactionsCopy).handleGeneration(extraNonce);
-    expect(transaction[0].slice(0, -5)).toStrictEqual(Buffer.from('01000000010000000000000000000000000000000000000000000000000000000000000000ffffffff0f5104', 'hex'));
-    expect(transaction[1]).toStrictEqual(Buffer.from('0000000002a0565009000000001976a914b92c9db55f187b0d7f11e7c2d82e1cdc74301ce388ac0000000000000000266a24aa21a9ede2f61c3f71d1defd3fa999dfa36953755c690689799962b48bebd836974e8cf900000000', 'hex'));
   });
 });
